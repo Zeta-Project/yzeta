82cf802f15b121a2b92ac6a050a55ee9ff9e9356
Manuele Lorusso <manuele.lorusso@gmail.com>
2019-09-17 10:56:16 +0200

    resolved #2 Drag and Drop example Elements -> reformat code

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,15 +1,15 @@
 import 'yfiles/yfiles.css';
 
 import {
-  Class,
-  FreeNodePortLocationModel,
-  GraphComponent,
-  GraphEditorInputMode,
-  ICommand,
-  LayoutExecutor,
-  License,
-  Point,
-  Rect
+    Class,
+    FreeNodePortLocationModel,
+    GraphComponent,
+    GraphEditorInputMode,
+    ICommand,
+    LayoutExecutor,
+    License,
+    Point,
+    Rect
 } from 'yfiles'
 
 import {bindCommand} from "./utils/Bindings";
@@ -24,64 +24,64 @@ Class.ensure(LayoutExecutor);
 
 
 /**
-* A simple yFiles application that creates a GraphComponent and enables basic input gestures.
-*/
+ * A simple yFiles application that creates a GraphComponent and enables basic input gestures.
+ */
 class YFilesZeta {
 
-  initialize() {
-    const graphComponent = new GraphComponent('#graphComponent');
-    const graph = graphComponent.graph;
-    graph.undoEngineEnabled = true
+    constructor() {
+        this.initialize();
+    }
 
-    const node1 = graph.createNode(new Rect(0, 0, 30, 30));
-    const node2 = graph.createNode(new Rect(100, 0, 30, 30));
-    const node3 = graph.createNode(new Rect(300, 300, 60, 30));
+    initialize() {
+        const graphComponent = new GraphComponent('#graphComponent');
+        const graph = graphComponent.graph;
+        graph.undoEngineEnabled = true
 
-    const edge1 = graph.createEdge(node1, node2);
-    const edge2 = graph.createEdge(node2, node3);
+        const node1 = graph.createNode(new Rect(0, 0, 30, 30));
+        const node2 = graph.createNode(new Rect(100, 0, 30, 30));
+        const node3 = graph.createNode(new Rect(300, 300, 60, 30));
 
-    const bend1 = graph.addBend(edge2, new Point(330, 15));
+        const edge1 = graph.createEdge(node1, node2);
+        const edge2 = graph.createEdge(node2, node3);
 
-    const portAtNode1 = graph.addPort(node1);
-    const portAtNode3 = graph.addPort(node3, FreeNodePortLocationModel.NODE_LEFT_ANCHORED);
-    const edgeAtPorts = graph.createEdge(portAtNode1, portAtNode3);
+        const bend1 = graph.addBend(edge2, new Point(330, 15));
 
-    const ln1 = graph.addLabel(node1, 'n1');
-    const ln2 = graph.addLabel(node2, 'n2');
-    const ln3 = graph.addLabel(node3, 'n3');
-    const le3 = graph.addLabel(edgeAtPorts, 'edgeAtPorts');
+        const portAtNode1 = graph.addPort(node1);
+        const portAtNode3 = graph.addPort(node3, FreeNodePortLocationModel.NODE_LEFT_ANCHORED);
+        const edgeAtPorts = graph.createEdge(portAtNode1, portAtNode3);
 
-    graphComponent.inputMode = new GraphEditorInputMode({
-        allowGroupingOperations: false,
-        allowCreateNode: false
-    });
+        const ln1 = graph.addLabel(node1, 'n1');
+        const ln2 = graph.addLabel(node2, 'n2');
+        const ln3 = graph.addLabel(node3, 'n3');
+        const le3 = graph.addLabel(edgeAtPorts, 'edgeAtPorts');
 
-    // configure and initialize drag and drop panel
-    let dragAndDropPanel = new DragAndDrop(graphComponent);
+        graphComponent.inputMode = new GraphEditorInputMode({
+            allowGroupingOperations: false,
+            allowCreateNode: false
+        });
 
-    graphComponent.fitGraphBounds();
+        // configure and initialize drag and drop panel
+        let dragAndDropPanel = new DragAndDrop(graphComponent);
 
-    // bind toolbar commands
-    this.registerCommands(graphComponent)
-  }
+        graphComponent.fitGraphBounds();
 
-  constructor() {
-    this.initialize();
-  }
+        // bind toolbar commands
+        this.registerCommands(graphComponent)
+    }
 
-  /**
-   * Wires up the UI.
-   * @param {GraphComponent} graphComponent
-   */
-  registerCommands(graphComponent) {
-    bindCommand("button[data-command='Cut']", ICommand.CUT, graphComponent)
-    bindCommand("button[data-command='Copy']", ICommand.COPY, graphComponent)
-    bindCommand("button[data-command='Paste']", ICommand.PASTE, graphComponent)
-    bindCommand("button[data-command='FitContent']", ICommand.FIT_GRAPH_BOUNDS, graphComponent)
-    bindCommand("button[data-command='ZoomOriginal']", ICommand.ZOOM, graphComponent, 1.0)
-    bindCommand("button[data-command='Undo']", ICommand.UNDO, graphComponent)
-    bindCommand("button[data-command='Redo']", ICommand.REDO, graphComponent)
-  }
+    /**
+     * Wires up the UI.
+     * @param {GraphComponent} graphComponent
+     */
+    registerCommands(graphComponent) {
+        bindCommand("button[data-command='Cut']", ICommand.CUT, graphComponent)
+        bindCommand("button[data-command='Copy']", ICommand.COPY, graphComponent)
+        bindCommand("button[data-command='Paste']", ICommand.PASTE, graphComponent)
+        bindCommand("button[data-command='FitContent']", ICommand.FIT_GRAPH_BOUNDS, graphComponent)
+        bindCommand("button[data-command='ZoomOriginal']", ICommand.ZOOM, graphComponent, 1.0)
+        bindCommand("button[data-command='Undo']", ICommand.UNDO, graphComponent)
+        bindCommand("button[data-command='Redo']", ICommand.REDO, graphComponent)
+    }
 
 }
 
