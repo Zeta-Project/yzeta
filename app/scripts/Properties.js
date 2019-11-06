0507fc5af35da50ee34f25cebff49fd2a91172d4
Daniel <danielkoch3@web.de>
2019-11-06 12:07:38 +0100

    added properties panel, just visuals

--- app/scripts/Properties.js
+++ app/scripts/Properties.js
@@ -12,14 +12,101 @@ import {
 
 export class Properties {
 
+
+
     constructor(graphComponent) {
         // retrieve the panel element
-        const panel = document.getElementById('properties-panel')
+        this.divField = document.getElementById('properties-panel')
+
+        const accordionMeta = document.createElement('button')
+        accordionMeta.className = 'accordion'
+        accordionMeta.innerHTML = 'Attributes'
+        const pMeta = document.createElement('p')
+        pMeta.class = 'panel'
+        pMeta.innerHTML = 'Some Meta Information'
+
+        const accordionAttributes = document.createElement('button')
+        accordionAttributes.className = 'accordion'
+        accordionAttributes.innerHTML = 'Attributes'
+
+        const pAttributes = document.createElement('p')
+        pAttributes.class = 'panel'
+        pAttributes.innerHTML = 'InnerTestP'
+
+        const accordionOperations = document.createElement('button')
+        accordionOperations.className = 'accordion'
+        accordionOperations.innerHTML = 'Operations'
+
+        const pOperations = document.createElement('p')
+        pOperations.class = 'panel'
+        pOperations.innerHTML = 'SomeOperations'
+
+
+
+        this.div.appendChild(accordionMeta)
+        this.div.appendChild(pMeta)
+        this.div.appendChild(accordionAttributes)
+        this.div.appendChild(pAttributes)
+        this.div.appendChild(accordionOperations)
+        this.div.appendChild(pOperations)
+
+        let acc = document.getElementsByClassName('accordion'); //cant find Elements
+        console.log(acc.length)
+        for (let i = 0; i < acc.length; i++) {
+            acc[i].addEventListener("click", function() {
+                /* Toggle between adding and removing the "active" class,
+                to highlight the button that controls the panel */
+                this.classList.toggle("active");
+
+                /* Toggle between hiding and showing the active panel */
+                let panel = this.nextElementSibling;
+                if (panel.style.display === "block") {
+                    panel.style.display = "none";
+                } else {
+                    panel.style.display = "block";
+                }
+            });
+        }
+    }
+
+    get div() {
+        return this.divField
+    }
+
+
+    updateProperties() {
+
     }
 
 }
 
 /**
+ * 1. build empty properties-panel
+ * 2. onClick Node -> send node
+ * 3. build apropriate accordion
+ *
+ *
+ *  <button class="accordion">Section 1</button>
+ <div class="panel">
+ <p>Lorem ipsum...</p>
+ </div>
+
+ <button class="accordion">Section 2</button>
+ <div class="panel">
+ <p>Lorem ipsum...</p>
+ </div>
+
+ <button class="accordion">Section 3</button>
+ <div class="panel">
+ <p>Lorem ipsum...</p>
+ </div>
+ *
+ * const div = document.createElement('div')
+ div.setAttribute('class', 'dndPanelItem')
+ div.appendChild(visual)
+ *
+ *
+
  constructor(graphComponent) {
         // Obtain the input mode for handling dropped nodes from the GraphEditorInputMode.
         const nodeDropInputMode = graphComponent.inputMode.nodeDropInputMode
